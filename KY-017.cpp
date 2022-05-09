d991a8db243aef3e7ed42898d843c80a62554185
Apollonoff <apollonoff.arseniy@gmail.com>
2022-05-09 14:19:12 +0300

    Функция спуска-подьема-толстая_черная_ПОЛОСА

--- KY-017.cpp
+++ KY-017.cpp
@@ -1,22 +1,58 @@
-int ledPin = 13;
-int tiltPin = 3;
-int val;
+// int ledPin = 13;
+// int tiltPin = 3;
+// int val;
 
-void setup()
-{
-    pinMode(ledPin, OUTPUT);
-    pinMode(tiltPin, INPUT);
+// void setup()
+// {
+//     pinMode(ledPin, OUTPUT);
+//     pinMode(tiltPin, INPUT);
+// }
+
+// void loop()
+// {
+//     val = digitalRead(tiltPin);
+//     if(val == HIGH)
+//     {
+//         digitalWrite(ledPin, HIGH);
+//     }
+//     else
+//     {
+//         digitalWrite(ledPin, LOW);
+//     }
+// }
+
+int led = 13;
+int shockSensor = 12;
+int value;
+
+void setup(){
+    pinMode(led, OUTPUT);
+    pinMode(shockSensor, INPUT);
+
 }
 
-void loop()
+void OvercomingRavine()
 {
-    val = digitalRead(tiltPin);
-    if(val == HIGH)
+    value = digitalRead(shockSensor);
+    if (value == HIGH) && (left && lmid && rmid && right)
     {
-        digitalWrite(ledPin, HIGH);
+        
+        digitalWrite(led, LOW);
+        //Замедление скорости машинки
+        analogWrite(SPEED_LEFT, 50 );
+        analogWrite(SPEED_RIGHT, 50);
+        delay(500);
+    } 
+    else if (left && lmid && rmid && right) {
+        //Ускорение при подьеме
+        digitalWrite(led, HIGH);
+        analogWrite(SPEED_LEFT, 200);
+        analogWrite(SPEED_RIGHT, 200);
+        delay(500);
     }
-    else
-    {
-        digitalWrite(ledPin, LOW);
+    else {
+        //Горизонтальные поверхности
+        digitalWrite(SPEED_LEFT, SPEED);
+        digitalWrite(SPEED_RIGHT, SPEED); 
     }
 }
